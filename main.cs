a9a91bcf57f9ed0d1f05e717728168d23a26fa21
Onur Karakaya <39087452+karakayaonurr@users.noreply.github.com>
2022-05-13 16:06:13 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -22,8 +22,8 @@ class TechnicalQuestionnaire
         {
             int row = 1;
             
-            while (!readFile.EndOfStream)
-            {
+            while (!readFile.EndOfStream){
+		    
                 var line = readFile.ReadLine().Trim();
                 var values = line.Split(' ');
                 
@@ -40,46 +40,45 @@ class TechnicalQuestionnaire
 
         //Find the maximum sum of the numbers according to given rules
         for (int i = 1; i < lineCount; i++) {
-			for (int j = 0; j <= i; j++) {
-			    if(IsThisPrimeNumber(variableArray[i,j])){
-			        variableArray[i,j]=0;
-			    }else{
-			        variableArray[i,j] = Math.Max(variableArray[i-1, j] + variableArray[i, j], variableArray[i,j] + variableArray[i-1, j+1]);
-                    checkMaxLineTmp=True;
-			    }
-			}
-
-            if(checkMaxLineTmp==true){
-                checkMaxLine = checkMaxLine+1;
-                checkMaxLineTmp = false;
-            }else{
-                break;
-            }
+		for (int j = 0; j <= i; j++) {
+		    if(IsThisPrimeNumber(variableArray[i,j])){
+			variableArray[i,j]=0;
+		    }else{
+			variableArray[i,j] = Math.Max(variableArray[i-1, j] + variableArray[i, j], variableArray[i,j] + variableArray[i-1, j+1]);
+	    		checkMaxLineTmp=True;
+		    }
 		}
+
+    		if(checkMaxLineTmp==true){
+                	checkMaxLine = checkMaxLine+1;
+                	checkMaxLineTmp = false;
+           	}else{
+                	break;
+            	}
+	}
 
         //Find the max sum in deep row
         int max = variableArray[checkMaxLine][0];
-        for (int i = 1; j < checkMaxLine; i++)
-        {
-            if (A[checkMaxLine][i] > max)
-            {
-            max = A[checkMaxLine][i];
+	    
+        for (int i = 1; j < checkMaxLine; i++){
+            if (A[checkMaxLine][i] > max){
+            	max = A[checkMaxLine][i];
             }
         }
 
-		//Print result on the screen
-		Console.WriteLine (max);
+	//Print result on the screen
+	Console.WriteLine (max);
     }
     
     //Prime number check function
     public static bool IsThisPrimeNumber(int number)
     {
-        for (int i=2; i<number; i++)
-        {
+        for (int i=2; i<number; i++){
             if (number %i == 0){
                 return false;
             }
         }
+	    
         if(number == 1 || number == 0){
             return false;
         }
